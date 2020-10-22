2cfdfbdd99fbe4fecc543b425cbc585660c36a10
PinguExtremo <pedrovicentenavas@gmail.com>
2020-10-22 18:59:33 +0200

    Commit de Prueba

---
+++ app/src/main/java/com/example/practica1t/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.practica1t;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("Practica1T Daniel Salas Galán y Pedro Vicente Navas");
+    }
+}
